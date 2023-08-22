141c98adbf4daa8f32bdf1828bce4b9369db5717
facundo <139131822+Cabclli@users.noreply.github.com>
2023-08-22 13:55:10 -0300

    Update index.js

--- array/array_4/index.js
+++ array/array_4/index.js
@@ -1,12 +1,12 @@
 const vuelta = [1,2,3,-5,-15]
 
 function cambio(num) {
-    numeros = []
+    let numeros = []
      
 
     for (let i = 0 ; i < num.length; i++){
 
-        numero = Math.abs(num[i])
+        const numero = Math.abs(num[i])
         
         numeros.push(numero)
 
@@ -15,5 +15,4 @@ function cambio(num) {
     };
     console.log(numeros)
 }
-
-console.log(vuelta)
+cambio(vuelta)
