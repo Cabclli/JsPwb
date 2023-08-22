e2277fe852e9597950d838eaa34712ff7b33cb9a
facundo caballi <facundocaballi@gmail.com>
2023-08-22 16:48:15 -0300

    actividad 2 de objetos

---
+++ objetos/actividad2/index.js
@@ -0,0 +1,24 @@
+// se imprime weight y height porque lo llama
+/*const person = {
+  name: "John",
+  age: 30,
+  hobbies: ["reading", "music", "sports"],
+};
+
+const { weight = 70, height = 180 } = person;
+console.log(weight, height);*/
+
+//no funciona porque la variable age no se encuentra definidad por fuera de esta funcion
+
+/*const person = {
+  name: "John",
+  age: 30,
+  hobbies: ["reading", "music", "sports"],
+};
+
+const { age: personAge } = person;
+console.log(age);
+*/
+
+//3- ¿Cómo se llama el atajo que nos permite recuperar el valor de una propiedad de un objeto?
+// se llama "Destructuración", En la anterior clase hemos visto que para recuperar una propiedad de un objeto podemos usar la notación de corchetes o la notación de punto
